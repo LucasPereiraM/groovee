84d8a4d61f05ca1f30f09230e6c77c2ea42c5350
LucasPereiraM <lucasmacielptu@gmail.com>
2022-11-28 12:01:14 -0300

    mudança na estrutura

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,25 @@
 <?php
+class User{
+    public $nome;
+    public $email;
+    public $senha;
+   
+    function __construct($nome, $email, $senha){
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+    function get_nome() {
+        return $this->nome;
+    }
+    function get_email() {
+        return $this->email;
+    }
+    function get_senha() {
+        return $this->senha;
+    }
+}
+$user = new User($_POST["nome"], $_POST["email"], $_POST["senha"]);
 echo $user->get_nome();
 echo $user->get_email();
 echo $user->get_senha();
