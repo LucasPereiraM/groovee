06003944ae92af2ecaa7d0d3bb9e88015e899e05
LucasPereiraM <lucasmacielptu@gmail.com>
2022-12-21 13:44:22 -0300

    🐞 fix: erro bd fix

--- conexao.php
+++ conexao.php
@@ -10,7 +10,7 @@
     $connMusic = new mysqli($host,$usuario,$senha,$databaseMusic);
 
     if($conn->error || $connMusic->error){
-        die("Falha de conexão ao banco de dados: " . $mysqli->error);
+        die("Falha de conexão ao banco de dados: " . $conn->error . "<br>" . $connMusic->error);
     }
 
 ?>
